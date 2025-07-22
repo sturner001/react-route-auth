79bc74254ec7385518fe90674c399bd6dd6f1435
Sam Turner <sam.turner@kokadjo.net>
2025-07-22 13:52:01 -0400

    implemented auth action

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams  } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+const [searchParams, setSearchParams] = useSearchParams();
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+const isLogin = searchParams.get('mode') === 'login'; 
+
+
+
 
   return (
     <>
@@ -23,9 +23,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin ? 'signin': 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </Link>
           <button>Save</button>
         </div>
       </Form>
